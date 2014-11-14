a585295475f802c4e6f77d45dd253d4b210372c5
Bamatembera <ak68250@logti-a3412-17.local>
2014-11-13 21:07:57 -0500

    topic menu, subject menu and starting question menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,23 @@
 $(function(){
 
-$("#search_bar_menu a").on("click",function(){
-	$("#topic").html($(this).html());
-});
+	$("#search_bar_menu a").on("click",function(){
+		$("#topic").html($(this).html());
+	});
 
-$('#topic_tabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+	$('#topic_tabs a').click(function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	});
 
+	$("#topic_list #topic_tabs li").click(function(){
+		$(".topic_list").hide();
+		$("#"+$(this).attr("data-list")).show();
+		$("#topic_list .topic_list .list-group a.list-group-item.active").removeClass("active");
+		$("#"+$(this).attr("data-list")).find(".list-group a.list-group-item:first").addClass("active");
+	});
+
+	$("#topic_list .topic_list .list-group a.list-group-item").click(function(){
+		$("#topic_list .topic_list .list-group a.list-group-item.active").removeClass("active");
+		$(this).addClass("active");
+	});
 });
