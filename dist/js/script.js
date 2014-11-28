42d77ba76c9165f1e20fe19aa0d5f83acad6af4e
Morais <ak94000@logti-a3412-17.local>
2014-11-27 19:58:16 -0500

    login register cookie question

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,17 @@
 $(function(){
+	if(getCookie("login")){
+		username = getCookie("login");
+		$("#navigation li").eq( 0 ).remove();
+		$("#navigation li").eq( 0 ).remove();
+		$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+username+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+		$("#user-menu").show();
+		$("#answer-question").show();
+		$("#logout").click(function(){
+			setCookie("login", "", "-1");
+			window.location.reload();
+		});
+	}
+
 	$("#search_bar_menu a").on("click",function(){
 		$("#topic").html($(this).html());
 	});
@@ -23,4 +36,46 @@ $(function(){
 		$(".subject_list").hide();
 		$("#"+$(this).attr("data-subject")).show();
 	});
+
+	$("#login").click(function(){
+		setCookie("login",$("#username").val(),1);
+		$("#navigation li").eq( 0 ).remove();
+		$("#navigation li").eq( 0 ).remove();
+		$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#username").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+		$('#myModal').modal('hide'); 
+		$("#user-menu").show();
+		$("#answer-question").show();
+		$("#logout").click(function(){
+			setCookie("login", "", "-1");
+			window.location.reload();
+		});
+	});
+
+	$("#logout").click(function(){
+		setCookie("login", "", "-1");
+		window.location.reload();
+	});
+
+	$("#btn-answer").click(function(){
+		$("#allAnswers tbody").prepend("<tr><td><i>-</i>0<i>+</i></td><td>"+$("#txt-answer").val()+"</td></tr><tr><td colspan=2 ><hr></td></tr>");
+		
+	});
 });
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
