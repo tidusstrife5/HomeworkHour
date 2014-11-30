e7f8a4fd675b8c90a68741bed86d0c9325313ee7
unknown <ak68250@logti-a4422-09w.ens.ad.etsmtl.ca>
2014-11-30 14:44:01 -0500

    Register, create question

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,8 +17,8 @@ $(function(){
 	});
 
 	$('#topic_tabs a').click(function (e) {
-	  e.preventDefault()
-	  $(this).tab('show')
+		e.preventDefault()
+		$(this).tab('show')
 	});
 
 	$("#topic_list #topic_tabs li").click(function(){
@@ -37,18 +37,49 @@ $(function(){
 		$("#"+$(this).attr("data-subject")).show();
 	});
 
+	$("#submit-question").click(function(){
+		timeout = window.setTimeout(function(){
+			window.location.href = "index.html";
+		},3000);
+	});
+
 	$("#login").click(function(){
-		setCookie("login",$("#username").val(),1);
-		$("#navigation li").eq( 0 ).remove();
-		$("#navigation li").eq( 0 ).remove();
-		$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#username").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
-		$('#myModal').modal('hide'); 
-		$("#user-menu").show();
-		$("#answer-question").show();
-		$("#logout").click(function(){
-			setCookie("login", "", "-1");
-			window.location.reload();
-		});
+		if($("#username").val()==""){
+			$("#username").addClass("error");
+		}
+		else{
+			setCookie("login",$("#username").val(),1);
+			$("#navigation li").eq( 0 ).remove();
+			$("#navigation li").eq( 0 ).remove();
+			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#username").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+			$('#myModal').modal('hide'); 
+			$("#user-menu").show();
+			$("#answer-question").show();
+			$("#logout").click(function(){
+				setCookie("login", "", "-1");
+				window.location.reload();
+			});
+		}
+	});
+
+
+	$("#register").click(function(){
+		if($("#regUname").val()==""){
+			$("#regUname").addClass("error");			
+		}
+		else{
+			setCookie("login",$("#regUname").val(),1);
+			$("#navigation li").eq( 0 ).remove();
+			$("#navigation li").eq( 0 ).remove();
+			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#regUname").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+			$('#myRegisterModal').modal('hide'); 
+			$("#user-menu").show();
+			$("#answer-question").show();
+			$("#logout").click(function(){
+				setCookie("login", "", "-1");
+				window.location.reload();
+			});
+		}
 	});
 
 	$("#logout").click(function(){
@@ -60,22 +91,23 @@ $(function(){
 		$("#allAnswers tbody").prepend("<tr><td><i>-</i>0<i>+</i></td><td>"+$("#txt-answer").val()+"</td></tr><tr><td colspan=2 ><hr></td></tr>");
 		
 	});
+
 });
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-    }
-    return "";
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+	}
+	return "";
 }
