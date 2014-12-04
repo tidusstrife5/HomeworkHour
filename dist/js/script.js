1b3920dc1ccc064f822f202b7b8fcbfdde7a41f3
Morais <ak94000@logti-a3412-17.local>
2014-12-04 18:00:27 -0500

    all

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,7 @@ $(function(){
 		username = getCookie("login");
 		$("#navigation li").eq( 0 ).remove();
 		$("#navigation li").eq( 0 ).remove();
-		$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+username+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+		$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='profile.html'>"+username+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
 		$("#user-menu").show();
 		$("#answer-question").show();
 		$("#logout").click(function(){
@@ -51,7 +51,7 @@ $(function(){
 			setCookie("login",$("#username").val(),1);
 			$("#navigation li").eq( 0 ).remove();
 			$("#navigation li").eq( 0 ).remove();
-			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#username").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='profile.html'>"+$("#username").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
 			$('#myModal').modal('hide'); 
 			$("#user-menu").show();
 			$("#answer-question").show();
@@ -71,7 +71,7 @@ $(function(){
 			setCookie("login",$("#regUname").val(),1);
 			$("#navigation li").eq( 0 ).remove();
 			$("#navigation li").eq( 0 ).remove();
-			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='#'>"+$("#regUname").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
+			$("#navigation").prepend("<li class='loggedin'>Bienvenue: <a href='profile.html'>"+$("#regUname").val()+"</a></li><li><a href='#' id='logout'>Logout</a></li>");
 			$('#myRegisterModal').modal('hide'); 
 			$("#user-menu").show();
 			$("#answer-question").show();
@@ -88,10 +88,29 @@ $(function(){
 	});
 
 	$("#btn-answer").click(function(){
-		$("#allAnswers tbody").prepend("<tr><td><i>-</i>0<i>+</i></td><td>"+$("#txt-answer").val()+"</td></tr><tr><td colspan=2 ><hr></td></tr>");
-		
+		$("#allAnswers tbody").prepend("<tr><td class='vote'><i class='glyphicon glyphicon-plus'></i><br><span class='voteNum'>0</span><br><i class='glyphicon glyphicon-minus'></i></td><td>"+$("#txt-answer").val()+"</td></tr><tr><td colspan=2 ><hr></td></tr>");
 	});
 
+	$(".vote .glyphicon-plus").click(function(){
+		$(this).next().next().eq(0).html((parseInt($(this).next().next().eq(0).html())+1));
+	});
+
+	$(".vote .glyphicon-minus").click(function(){
+		$(this).prev().prev().eq(0).html((parseInt($(this).prev().prev().eq(0).html())-1));
+	});
+
+	$(".fr .glyphicon-remove").click(function(){
+		a=confirm("Do you really want to delete this question?");
+		if(a){
+			$(this).parent().parent().addClass("error");
+			$(this).parent().parent().css("margin-bottom","10px");
+			$(this).parent().parent().fadeOut(1000);
+			setTimeout(function() {$(this).parent().parent().remove();}, 1000);	
+		}
+	});
+	$(".fr .glyphicon-pencil").click(function(){
+		$(this).parent().parent().remove();
+	});
 });
 
 function setCookie(cname, cvalue, exdays) {
